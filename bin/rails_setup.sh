493005ef96f804fd5407d34679e66bf21f90b87f
ressy-private <tsuyoshi431@gmail.com>
2017-08-14 17:19:33 +0900

    update rails_setup.sh
    To resolve warning messages when the rails_setup.sh is run.

--- bin/rails_setup.sh
+++ bin/rails_setup.sh
@@ -3,22 +3,25 @@
 USER=rails
 
 # rbenvのインストール
-INSTALL_RBENV=$(cat <<EOS
+INSTALL_RBENV=$(cat <<'EOS'
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo "export PATH=~/.rbenv/shims:~/.rbenv/bin:$PATH" >> ~/.bash_profile
-echo "eval \"$(rbenv init -)\"" >> ~/.bash_profile
+echo "eval \"\$(rbenv init -)\"" >> ~/.bash_profile
+echo "cd ~" >> ~/.bash_profile
 source ~/.bash_profile
-EOS)
+EOS
+)
 su -l "${USER}" -c "${INSTALL_RBENV}"
 
 # ruby-buildのインストール
 /home/"${USER}"/.rbenv/plugins/ruby-build/install.sh
 
 # rubyのインストール
-INSTALL_RUBY=$(cat <<EOS
+INSTALL_RUBY=$(cat <<'EOS'
 rbenv install 2.4.1
 rbenv global 2.4.1
 gem install bundler
-EOS)
+EOS
+)
 su -l "${USER}" -c "${INSTALL_RUBY}"
